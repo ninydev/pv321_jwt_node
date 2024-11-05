7802e9a22fb6df8ab3636ad702d4b32e3b45dc2d
Oleksandr Nykytin <keeper@ninydev.com>
2024-11-05 19:59:47 +0200

    Docker update

--- docker-project/backend/express-api/src/index.js
+++ docker-project/backend/express-api/src/index.js
@@ -8,10 +8,14 @@ import {handleRoute2} from "./controllers/route2.js";
 const app = express();
 
 // Первый маршрут
-app.get('/route/1', handleRoute1);
+app.get('/api/express/first', handleRoute1);
 
 // Второй маршрут
-app.get('/route/2', handleRoute2);
+app.get('/api/express/second', handleRoute2);
+
+app.get('*', (reg, res) => {
+    res.send('Any Other');
+});
 
 app.listen(SERVER_PORT, SERVER_HOST, () => {
     console.log(`HTTP Server '${SERVER_NAME}' is running on http://${SERVER_HOST}:${SERVER_PORT}`);
