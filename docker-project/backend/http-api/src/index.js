8cdf8286eacf312b0127bc702fec9c43d8b23a1b
Oleksandr Nykytin <keeper@ninydev.com>
2024-11-07 20:24:43 +0200

    Add Cache Redis

--- docker-project/backend/http-api/src/index.js
+++ docker-project/backend/http-api/src/index.js
@@ -2,25 +2,29 @@ const SERVER_NAME = process.env.SERVER_NAME || "http module";
 const SERVER_HOST = process.env.SERVER_HOST || "0.0.0.0";
 const SERVER_PORT = process.env.SERVER_PORT || 3000;
 
+const REDIS_HOST = process.env.REDIS_HOST || 'localhost';
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+const REDIS_CACHE_DB = process.env.REDIS_CACHE_DB || 0;
+const CACHE_TTL = process.env.CACHE_TTL || 9999999;
+
+import { Keyv } from 'keyv';
+import KeyvRedis from '@keyv/redis';
 import { createCache } from 'cache-manager';
-import redisStore from 'cache-manager-redis-store';
 
 const cache = createCache({
-    store: redisStore,
-    host: 'vp321.redis', // имя контейнера Redis в docker-compose
-    db: 0,
-    port: 6379,   // порт Redis по умолчанию
-    ttl: 60000000 // Время жизни кэша в секундах
+    stores: [
+        //  Redis Store
+        new Keyv({
+            store: new KeyvRedis(`redis://${REDIS_HOST}:${REDIS_PORT}/${REDIS_CACHE_DB}`),
+        }),
+    ],
+})
+
+console.log(`redis://${REDIS_HOST}:${REDIS_PORT}/${REDIS_CACHE_DB}`)
+
+cache.on('set', (args) => {
+    console.info('Redis set:', args);
 });
-
-
-
-// Bad
-// const cache = createCache( {
-//     store: 'memory',
-//     max: 100,
-//     ttl: 606868686
-// });
 
 
 import { createServer } from "http";
@@ -32,15 +36,21 @@ const httpServer
 
     let msg = `<html><body><ul><li>SERVER_NAME = ${SERVER_NAME}</li>`;
     msg += '<li> Cache Var:' + (await cache.get('Var')) + '</li>';
-    msg += '<li> Cache MainVar:' + (await cache.get('MainVar')) + '</li>';
+    msg += '<li> Cache ServerName:' + (await cache.get('ServerName')) + '</li>';
     msg += '</ul></body></html>';
 
     res.end(msg);
 })
 
 httpServer.listen(SERVER_PORT, SERVER_HOST,async () => {
-    await cache.set('Var', 'Value');
-    await cache.set('MainVar', SERVER_NAME);
+    cache.set('Var', 'Value', CACHE_TTL)
+        .then(d => {
+            console.log('then',d)
+        })
+        .catch(e => {
+            console.error(e)
+        });
+    await cache.set('ServerName', SERVER_NAME, CACHE_TTL);
 
     console.log(`HTTP Server '${SERVER_NAME}' is running on http://${SERVER_HOST}:${SERVER_PORT}`);
 });
